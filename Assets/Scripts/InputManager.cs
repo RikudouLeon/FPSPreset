075a1bdafbc5c92ba2ef24d980eeccca45ea2a5a
leon <leon.mahesa@gmail.com>
2023-01-07 00:29:10 +0100

    Added Functionality to WASD Input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+    private PlayerMovement movement;
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        movement = GetComponent<PlayerMovement>();
+    }
+
+    void FixedUpdate()
+    {
+        movement.ProcessMove(onFoot.Movement.ReadValue<Vector2>()); // Moves the player using value from movement action
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
