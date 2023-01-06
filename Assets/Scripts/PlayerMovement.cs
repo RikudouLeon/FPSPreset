d3a6626974e92912b9e51bcac89049ab4505a08b
leon <leon.mahesa@gmail.com>
2023-01-07 00:39:20 +0100

    Added Gravity Functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 playerVelocity;
     public float speed = 5f;
 
+    private bool isGrounded;
+    public float gravity = -9.8f;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,17 +18,29 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+        isGrounded = controller.isGrounded;
     }
 
     // Receives inputs for InputManager.cs and applies them to character controller
     public void ProcessMove (Vector2 input)
     {
+        // On foot movement
         Vector3 moveDirection = Vector3.zero;
 
         moveDirection.x = input.x;
         moveDirection.z = input.y;
 
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+        // Gravity
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
     }
 }
