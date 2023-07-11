df2fb4be57dee17c8a5a49f6a0f215b1f8182278
leonPCminjem <leon.mahesa@gmail.com>
2023-07-11 15:31:00 +0200

    Added Interaction Events
    - Tested events by changing door material when door keypad is interacted by the player

---
+++ Assets/Editor/InteractionEditor.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+
+[CustomEditor(typeof(Interaction), true)]
+public class InteractionEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Interaction interaction = (Interaction)target; // store an instance of the Interaction script. "target" is the currently selected game object that is inspected
+        base.OnInspectorGUI();
+        if (interaction.useEvents)
+        {
+            // If we are using events, add the component
+            if (interaction.GetComponent<InteractEvent>() == null)
+            {
+                interaction.gameObject.AddComponent<InteractEvent>();
+            }
+        }
+        // If we are not using events, remove the component
+        else
+        {
+            if (interaction.GetComponent<InteractEvent>() != null)
+            {
+                DestroyImmediate(interaction.GetComponent<InteractEvent>());
+            }
+        }  
+    }
+}
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Interaction : MonoBehaviour
 {
     // Display message to player when looking at an interactable object
-    public string promptMessage;
+    [SerializeField] public string promptMessage;
+    public bool useEvents; // Add or Remove an InteractEvent component to this game object
 
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
+    
     public void BaseInteract() // Called from our player
     {
+        if (useEvents)
+        {
+            GetComponent<InteractEvent>().OnInteract.Invoke(); // this value should never be null because an editor script is used to add the interaction event component
+        }
         Interact();
     }
 
